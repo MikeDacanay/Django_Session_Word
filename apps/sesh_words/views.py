316efc44519a0d2adf203453a28fe272b8472f49
Mike Dacanay <migd64@gmail.com>
2017-08-14 17:25:23 -0700

    sessions are working as intended, implemented localtime,strftime module intoadd method

--- apps/sesh_words/views.py
+++ apps/sesh_words/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, HttpResponse, redirect
+from time import localtime, strftime
 # the index function is called when root is visited
 def index(request):
-  response = "placeholder to later display all the list of blogs"
-  return HttpResponse(response)
+	return render(request, 'sesh_words/index.html')
+
+def add(request):
+	request.session['word']=request.POST['word_form']
+	print request.session['word']
+	context = {
+  	"time": strftime("added on %I:%M %p %b %d %Y", localtime())
+  	}
+  	# print context['time'] USE THE KEY TIME TO GET EXACT time word is added
+	return redirect("/")
