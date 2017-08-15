fe1ea1b814ca1a7f3c2f6dd573b8e6bb0ba23960
Mike Dacanay <migd64@gmail.com>
2017-08-14 20:36:22 -0700

    complete

--- apps/sesh_words/views.py
+++ apps/sesh_words/views.py
@@ -5,10 +5,26 @@ def index(request):
 	return render(request, 'sesh_words/index.html')
 
 def add(request):
-	request.session['word']=request.POST['word_form']
-	print request.session['word']
-	context = {
-  	"time": strftime("added on %I:%M %p %b %d %Y", localtime())
-  	}
-  	# print context['time'] USE THE KEY TIME TO GET EXACT time word is added
-	return redirect("/")
+
+    new_word = {}
+    for key, value in request.POST.iteritems():
+        if key != "csrfmiddlewaretoken" and key != "show-big":
+            new_word[key] = value
+        if key == "show-big":
+            new_word['big'] = "big"
+        else:
+            new_word['big'] = ""
+    new_word['created_at'] = datetime.now().strftime("%H:%M %p, %B %d, %Y")
+    try:
+        request.session['words']
+    except KeyError:
+        request.session['words'] = []
+
+    temp_list = request.session['words']
+    temp_list.append(new_word)
+    request.session['words'] = temp_list
+    for key, val in request.session.__dict__.iteritems():
+        print key, val
+    print "past craeted at", new_word
+
+    return redirect('/')
